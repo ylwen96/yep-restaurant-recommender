5c1d567c7ecb2cb109c67456e801dcffcb9b234a
Yuelin Wen <wenyuelinca@gmail.com>
2025-02-28 09:27:11 -0500

    implment leaflet map

--- controller/index.js
+++ controller/index.js
@@ -13,18 +13,6 @@ router.get("/", (req, res) => {
         })
 });
 
-// GET: /restaurants/search?name=name get by restaurant id
-router.get("/search", (req, res) => {
-    db.getRestaurantByName(req.query.page, req.query.perPage, req.query.name)
-        .then((data) => {
-            res.status(200).json(data);
-        })
-        .catch(err => {
-            res.status(400).json({ "message": "Resource not found" + err })
-        })
-});
-
-
 // GET: /restaurants/:id get by restaurant id
 router.get("/:id", (req, res) => {
     db.getRestaurantById(req.params.id)
